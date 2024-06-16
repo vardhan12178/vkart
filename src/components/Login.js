70e97d99ce65add3cf15176aac59cb3729e329b6
vardhan12178 <balavardhan12178@gmail.com>
2024-06-16 11:14:53 +0530

    added Feature in login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,13 @@
-// Login.js
 import React, { useState, useEffect } from 'react';
 import axios from './axiosInstance';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
+import { EyeIcon, EyeOffIcon } from '@heroicons/react/outline'; // Assuming you have heroicons installed
 
 const Login = ({ setIsLoggedIn }) => {
   const [userId, setUserId] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState('');
   const navigate = useNavigate();
 
@@ -42,6 +43,10 @@ const Login = ({ setIsLoggedIn }) => {
     }
   };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center p-4">
       <div className="max-w-4xl w-full bg-white shadow-md rounded-lg p-6 flex flex-col md:flex-row items-center md:items-start">
@@ -67,18 +72,32 @@ const Login = ({ setIsLoggedIn }) => {
               required
             />
           </div>
-          <div className="mb-4">
+          <div className="mb-4 relative">
             <label htmlFor="password" className="block text-gray-700">
               Password
             </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-              required
-            />
+            <div className="relative">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 pr-10" // Added padding to the right
+                required
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500 focus:outline-none"
+                style={{ right: '10px' }} // Adjusted positioning
+              >
+                {showPassword ? (
+                  <EyeOffIcon className="w-6 h-6" />
+                ) : (
+                  <EyeIcon className="w-6 h-6" />
+                )}
+              </button>
+            </div>
           </div>
           {error && <p className="text-red-500 mb-4">{error}</p>}
           <button
