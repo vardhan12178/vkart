daba4f76ecbc198a9088db82c1ad2f175da39e59
vardhan12178 <balavardhan12178@gmail.com>
2024-06-18 17:21:08 +0530

    Updated Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 py-4 fixed bottom-0 w-full mt-10">
+    <footer className="bg-blue-500 py-4 fixed bottom-0 w-full">
       <div className="container mx-auto text-center text-white">
-        <p className="text-gray-400">
+        <p className="text-sm">
           Vkart | Developed by BalaVardhan | 2024
         </p>
       </div>
